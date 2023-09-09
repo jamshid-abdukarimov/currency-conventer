035756ea2901a38096cd913780e0cabb40cbe5b6
jamshid-abdukarimov <jamshid20000521@gmail.com>
2023-09-09 12:28:33 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
     color: "#222",
     minHeight: "20rem",
     borderRadius: 2,
-    padding: "4rem 2rem",
+    padding: "2rem 1rem",
     boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
     position: "relative",
   };
